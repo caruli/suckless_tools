c16cc7e09e19b180aac58b86dd1692317e40aa11
caruli <caruli@gmail.com>
2022-05-19 07:48:15 +0200

    sdsd

---
+++ suckless/suckless.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#  dual window manager 2wm   26.4 kilo kbytes
+#git clone https://oldgit.suckless.org/2wm
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://github.com/bakkeby/dwm-flexipatch.git
+#git clone https://git.suckless.org/surf
+
+
